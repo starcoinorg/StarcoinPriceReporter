56425cfdce3e7f332c8a7552b903f0b7ee990128
yangjiefeng <yangjiefeng@gmail.com>
2021-09-13 14:13:39 +0800

    add cors config.

---
+++ src/main/java/org/starcoin/stcpricereporter/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package org.starcoin.stcpricereporter.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/**", config);
+        return new CorsFilter(configSource);
+    }
+
+}

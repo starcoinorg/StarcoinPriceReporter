9b9e97c6eab47fb3726311bc9fbe2cf3b217948d
yangjiefeng <yangjiefeng@gmail.com>
2021-09-18 21:31:52 +0800

    add ddl

--- src/main/resources/db/migration/V0.0.2__add_price_round_data.sql
+++ src/main/resources/db/migration/V0.0.2__add_price_round_data.sql
@@ -1,2 +1,13 @@
+CREATE TABLE `price_round` (
+  `pair_id` varchar(50) NOT NULL,
+  `round_id` decimal(50,0) NOT NULL,
+  `created_at` bigint(20) NOT NULL,
+  `created_by` varchar(70) NOT NULL,
+  `price` decimal(50,0) NOT NULL,
+  `updated_at` bigint(20) NOT NULL,
+  `updated_by` varchar(70) NOT NULL,
+  `version` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`pair_id`,`round_id`),
+  KEY `IdxPairIdUpdatedAt` (`pair_id`,`updated_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
 
--- todo

3660d1988a64d3a1e9fd03cfdc0a3fbf2ed3c8f2
yangjiefeng <yangjiefeng@gmail.com>
2021-09-19 15:53:24 +0800

    update DDL

--- src/main/resources/db/migration/V0.0.3__add_price_growth.sql
+++ src/main/resources/db/migration/V0.0.3__add_price_growth.sql
@@ -1,4 +1,15 @@
 alter table price_round add column started_at bigint;
 alter table price_round add column answered_in_round decimal(50,0);
 
--- todo create table...
+-- DROP TABLE IF EXISTS `price_growth`;
+CREATE TABLE `price_growth` (
+   `pair_id` varchar(50) NOT NULL,
+   `created_at` bigint(20) NOT NULL,
+   `created_by` varchar(70) NOT NULL,
+   `day_over_day_percentage` decimal(15,7) DEFAULT NULL,
+   `updated_at` bigint(20) NOT NULL,
+   `updated_by` varchar(70) NOT NULL,
+   `version` bigint(20) DEFAULT NULL,
+   PRIMARY KEY (`pair_id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
